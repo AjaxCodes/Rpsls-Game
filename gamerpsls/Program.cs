8ac7d8fec13b55cf68078cfdd5f921d9e65f1a14
unknown <adamjsteinbacher@gmail.com>
2020-06-11 09:24:23 -0500

    Initial commit

---
+++ gamerpsls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gamerpsls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
