afbad11567d484c6c419104792799e2afe316d46
unknown <adamjsteinbacher@gmail.com>
2020-06-12 09:06:52 -0500

    filling out classes with member variables, public constructors, making // notes on later steps to hold back errors

--- gamerpsls/Game.cs
+++ gamerpsls/Game.cs
@@ -8,22 +8,47 @@ namespace gamerpsls
     {
         // Member Variables
         string player1;
-        
+        string player2;
 
-        //player1.Choosegesture();
-        //player2.ChooseGesture();
+       
          public Game()
          {
-            //player1 = new Player();
-            //player1 = new Human();
+           //player1 = new Player();
+           //player1 = new Human();
+           //player1.Choosegesture();
+           //player2.ChooseGesture();           // Errors are till you define in other classes
+
+        }
+
+        public void DisplayRules() 
+        {
+            Console.WriteLine("The rules are");
+
+            Console.WriteLine("choose game mode");
+            Console.WriteLine("1 for human v ai");
+            Console.WriteLine("2 for human v human");
+
+            //if (userInput == "1")
+            {
+                //(make p2 an ai);
 
 
-         }     
-        
-        
-        
-        
-        
+            }
+            //else (userInput == "2");
+            {
+
+                //make p2 a human;        //(   swap values   )
+
+            }
+
+
+
+        }
+           
+
+
+
+
         // Constructor
 
         // Member Method
--- gamerpsls/Player.cs
+++ gamerpsls/Player.cs
@@ -6,6 +6,33 @@ namespace gamerpsls
 {
     public class Player
     {
+        string player1;
+        string player2;
+        string human;
+        string computer;
+
+
+
+
+        public Player() 
+        {
+
+
+            
+
+
+
+
+        }
+        //public abstract void ChooseGesture();     //(virtual if child)
+        //{
+
+
+
+       // }
+
+
+
 
 
 
